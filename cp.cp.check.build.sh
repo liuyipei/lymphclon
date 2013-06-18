0c6647f922d41347172191b9ce8977f4b2b914d4
Yi Liu <liu.yi.pei@gmail.com>
2013-06-17 21:30:44 -0700

    removed 'loo' options. created additional regularization options where the off diagonal elements target the min of vars amongst replicates; also, implemented a  simple james stein regularization chooser for that target

--- cp.cp.check.build.sh
+++ cp.cp.check.build.sh
@@ -4,3 +4,4 @@ cp lymphclon_man/* lymphclon/man/
 cp DESCRIPTION lymphclon/
 R CMD check lymphclon
 R CMD build lymphclon
+rm *.Rout
--- reinstall.lymphclon.R
+++ reinstall.lymphclon.R
@@ -1,3 +1,3 @@
 remove.packages('lymphclon')
-install.packages('lymphclon_1.0.12.tar.gz', repos = NULL, type = 'source')
+install.packages('lymphclon_1.0.17.tar.gz', repos = NULL, type = 'source')
 
